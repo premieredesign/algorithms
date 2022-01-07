21f98d6a5d68874a0441d573b3e0948cbb05b018
Clinton M. Johnson <premieredesign@icloud.com>
2022-01-07 11:41:58 -0500

    adding bubble sort
    Bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,17 @@
+const bubbleSort = (arr) => {
+    let swapped = false;
+    
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+                swapped = true;
+            };
+        };
+    } while(swapped) {
+    
+    }
+};
